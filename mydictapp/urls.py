fed37834624dbe5862718e6e2abe91255cef1b7a
Sarad3993 <risingstar3993@gmail.com>
2020-08-26 14:22:56 +0545

    project completed

--- mydictapp/urls.py
+++ mydictapp/urls.py
@@ -1,6 +1,13 @@
 from django.urls import path 
+from .views import *
+from django.conf.urls.static import static 
+from EngToNepDict.settings import DEBUG, MEDIA_ROOT,MEDIA_URL 
 
+app_name = 'mydictapp'
 
 urlpatterns = [
-    
+    path('',engtonepdict,name='engtonepdict'),
 ]
+
+if DEBUG:
+    urlpatterns += static(MEDIA_URL, document_root = MEDIA_ROOT)
--- mydictapp/views.py
+++ mydictapp/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from bs4 import BeautifulSoup 
+import requests # to send GET requests 
+ 
 
-# Create your views here.
+def engtonepdict(request):
+    if request.method == 'POST':
+        word = request.POST['word']
+        context = {'q':word}
+        req = requests.get('https://www.englishnepalidictionary.com/',params=context).text
+        soup = BeautifulSoup(req,'html.parser')
+        nepali_meaning = soup.find(class_='search-result').h3.text.strip()
+        context_view = {'nepali_meaning':nepali_meaning}
+        return render(request,'index.html',context_view)
+    return render(request,'index.html')
